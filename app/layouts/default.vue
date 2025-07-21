c7b13b6dee07290d0b19dff05e61c876a8a5d766
hiyongliz <644220198@qq.com>
2025-07-21 18:01:16 +0800

    init commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import DarkToggle from '~/components/DarkToggle.vue'
+</script>
+
+<template>
+  <main flex="~ col" w-full justify-center>
+    <div flex="~" gap-110 h-14 w-full items-center justify-center>
+      <a text-3xl href="/">Lazy's Webblog</a>
+      <div flex="~" gap-4 items-center>
+        <a href="/about">About</a>
+        <a i-carbon-logo-github href="/about" target="_blank" />
+        <DarkToggle />
+      </div>
+    </div>
+    <div h-6 w-full class="splitter" />
+    <div flex="~" justify-center>
+      <div w-800px>
+        <slot />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+.splitter {
+  background: linear-gradient(to bottom, rgb(33, 147, 176), rgb(109, 213, 237));
+}
+</style>
---
+++ app/pages/blog.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('blog', () => queryCollection('blog').all())
+console.log(posts)
+</script>
+
+<template>
+  <div>
+    <h1>Blog</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="post.path">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
